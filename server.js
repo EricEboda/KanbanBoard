7407f04061b7d048f3afd047dcf4485636de257f
Denise Namutebi <denisenamutebi@gmail.com>
2020-10-19 11:01:16 +0100

    moved files and cleared server

--- server.js
+++ server.js
@@ -1,14 +1,8 @@
 const express = require('express')
-const Handlebars = require('handlebars')
-const expressHandlebars = require('express-handlebars')
-const {allowInsecurePrototypeAccess} = require('@handlebars/allow-prototype-access')
 const app = express()
 
-const handlebars = expressHandlebars({
-    handlebars: allowInsecurePrototypeAccess(Handlebars)
-})
-
 app.use(express.static('public'))
-app.engine('handlebars', handlebars)
-app.set('view engine', 'handlebars')
+
+
+app.listen(3000, () => console.log("app running on localhost 3000"))
 
