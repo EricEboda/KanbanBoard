d8db1816d4d002f7cbe1983c5f67a816b2f33c6b
CodeEryk <ericjeb3@gmail.com>
2020-10-20 16:43:59 +0100

    Update version 3

--- server.js
+++ server.js
@@ -4,19 +4,9 @@ const app = express()
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 const path = require('path');
-<<<<<<< HEAD
 
 app.use(express.static(path.join(__dirname, 'public')))
 
-=======
-
-app.use(express.static(path.join(__dirname, 'public')))
-
-app.get('/user', async(req, res) => {
-    const users = await User.findAll()
-    res.send(users)
-})
->>>>>>> cac0d892a4ec003573688819145227021995e943
 
 
 app.listen(3000, () => console.log("app running on localhost 3000"))
