c6cee1cb89a78e92e7b24d58d83a7fa0481d5d5a
CodeEryk <ericjeb3@gmail.com>
2020-10-20 16:24:46 +0100

    Display Functionality

--- public/homepage.js
+++ public/homepage.js
@@ -1,2 +1,13 @@
-const boards = localStorage.getItem("boards") || [];
+let boards = localStorage.getItem("boards") || [];
 console.log(boards)
+
+for (let i = 0; i < boards.length; i++) {
+    console.log(boards.length)
+    boards = JSON.parse(localStorage.getItem("boards") || "[]");
+    console.log(boards.length)
+    let boardLink = document.createElement('a')
+    boardLink.innerHTML = `Board: ${boards[i].title}`
+    document.querySelector(".boardList").appendChild(boardLink)
+    boardLink.setAttribute("class", "boardDisplay")
+    boardLink.setAttribute("href", `/board.html?id=${boards[i].id}`);
+}
--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require('express')
+const {User, Board, Task, sequelize} = require('./model')
 const app = express()
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+const path = require('path');
 
-app.use(express.static('public'))
+app.use(express.static(path.join(__dirname, 'public')))
+
 
 
 app.listen(3000, () => console.log("app running on localhost 3000"))
