ef59f14485a0f6a9836f7618caa35881ca02e420
CodeEryk <ericjeb3@gmail.com>
2020-10-21 12:13:22 +0100

    When viewing boards tasks are now movable

--- public/homepage.js
+++ public/homepage.js
@@ -4,7 +4,7 @@ boards = JSON.parse(localStorage.getItem("boards") || "[]");
 for (let i = 0; i < boards.length; i++) {
     console.log(boards.length)
     let boardLink = document.createElement('a')
-    boardLink.innerHTML = `Board: ${boards[i].title}`
+    boardLink.innerHTML = boards[i].title
     document.querySelector(".boardList").appendChild(boardLink)
     boardLink.setAttribute("class", "boardDisplay")
     boardLink.setAttribute("href", `/board.html?id=${boards[i].id}`);
